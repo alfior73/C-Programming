30c5eb2fbba8e845e460cb025199481ad48fcb38
Alfio Raymond <alfior73@users.noreply.github.com>
2023-09-12 10:22:19 -0400

    Create datatypesInput.c

---
+++ datatypesInput.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int var1, var2 = 027;
+	int var3 = 0xD;
+	int var4;
+	
+	var4 = var2 + 5;
+	var1 = 30;
+	
+	scanf("%d %o %x\n", &var1, &var2, &var4);
+	
+	return 0;
+}
