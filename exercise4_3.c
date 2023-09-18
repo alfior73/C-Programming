d9b95a770c55924f2a789effaf74a30de024901c
Alfio Raymond <alfior73@users.noreply.github.com>
2023-09-18 14:37:24 -0400

    Create exercise4_3.c

---
+++ exercise4_3.c
@@ -0,0 +1,16 @@
+/* Sample Exercise 3
+
+   Given the a factorial is the product of consecutive
+   integers 1 through n, write a program to calculate the
+   first 10 factorial values
+
+   2 factorial = 1 * 2
+
+   3 factorial = 1 * 2 * 3
+
+   4 factorial = 1 * 2 * 3 * 4
+
+   Hint:  Perfect place to use any loop ...
+
+*/
+
